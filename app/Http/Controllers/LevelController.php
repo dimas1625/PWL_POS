0a47daadb479a6803a30f5975d3359666cc5c2bd
Dimas Nugroho <dimasceper358@gmail.com>
2025-03-07 10:05:55 +0700

    praktikum-4.7-9

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -10,12 +10,16 @@ class LevelController extends Controller
     public function index()
     {
         
-        //DB::insert('insert into m_level (level_kode, level_nama, created_at) values (?, ?, ?)', ['CUS', 'Pelanggan', now()]);
-        //return 'Insert data baru berhasil';
+        // DB::insert('insert into m_level (level_kode, level_nama, created_at) values (?, ?, ?)', ['CUS', 'Pelanggan', now()]);
+        // return 'Insert data baru berhasil';
 
-        $row = DB::update('update m_level set level_nama = ? where level_kode = ?', ['Customer', 'CUS']);
+        // $row = DB::update('update m_level set level_nama = ? where level_kode = ?', ['Customer', 'CUS']);
+        // return 'Update data berhasil. Jumlah data yang diupdate: " . $row.' baris';
 
-        echo 'Update data berhasil. Jumlah data yang diupdate: ' . $row . ' baris.<br>';
-        return 'Update data berhasil. Jumlah data yang di update: ' . $row.' baris';
+        // $row = DB::delete('delete from m_level where level_kode - ?', ['CUs']);
+        // return 'Delete data berhasil. Jumlah data yang dihapus: ' . $row.' baris';
+
+        $data = DB::select('select * from m_level');
+        return view('level', ['data' => $data]);
     }
 }
