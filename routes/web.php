1c8a64655f273670cecdd5ab91ad73018ab0ab5c
Dimas Nugroho <dimasceper358@gmail.com>
2025-03-07 10:56:30 +0700

    praktikum-6.1-7

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,14 @@
 
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Pastikan setiap route sesuai dengan nama metode yang ada di Controller
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
