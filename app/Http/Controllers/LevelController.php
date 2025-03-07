87e8ffa77df1708d51eda747973f76bcefcb3f25
Dimas Nugroho <dimasceper358@gmail.com>
2025-03-07 09:50:01 +0700

    praktikum-4.5-6

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -3,17 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB; // Tambahkan ini
+use Illuminate\Support\Facades\DB;
 
 class LevelController extends Controller
 {
-    public function index() {
-        DB::insert('insert into m_level (level_kode, level_nama, created_at) values (?, ?, ?)', [
-            'CUs', 
-            'Pelanggan', 
-            now()
-        ]);
+    public function index()
+    {
+        
+        //DB::insert('insert into m_level (level_kode, level_nama, created_at) values (?, ?, ?)', ['CUS', 'Pelanggan', now()]);
+        //return 'Insert data baru berhasil';
 
-        return 'Insert data baru berhasil';
+        $row = DB::update('update m_level set level_nama = ? where level_kode = ?', ['Customer', 'CUS']);
+
+        echo 'Update data berhasil. Jumlah data yang diupdate: ' . $row . ' baris.<br>';
+        return 'Update data berhasil. Jumlah data yang di update: ' . $row.' baris';
     }
 }
