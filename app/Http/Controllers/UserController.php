6bb47cda47934024dd01c1b3c3426f412f691a9c
Dimas Nugroho <dimasceper358@gmail.com>
2025-03-11 17:16:58 +0700

    js4-praktikum2.1-4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
 {
     public function index(){
 
-        $user = UserModel::find(1); // ambil semua data dari tabel m_user
+        $user = UserModel::where('level_id', 1)->first(); // ambil semua data dari tabel m_user
         return view('user', ['data' => $user]);
     }   
 }
