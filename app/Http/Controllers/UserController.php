1eeddf1b01b9e6a38d24b46562e446813ad1ea3b
Dimas Nugroho <dimasceper358@gmail.com>
2025-03-11 17:13:17 +0700

    js4-praktikum2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,16 +9,8 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        
-        $data = [
-            'level_id' => '2',
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data); 
-        
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
+
+        $user = UserModel::find(1); // ambil semua data dari tabel m_user
         return view('user', ['data' => $user]);
     }   
 }
