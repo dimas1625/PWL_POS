3e84e817d36a146f2ddb3750e5031d8399f5d13b
Dimas Nugroho <dimasceper358@gmail.com>
2025-03-11 17:00:55 +0700

    js4-praktikum1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,13 +9,16 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        // tambah data user dengan Eloquent Model
+        
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'level_id' => '2',
+            'username' => 'manager dua',
+            'nama' => 'Manager 2',
+            'password' => Hash::make('12345')
         ];
-            UserModel :: where('username', 'customer-1')->update($data); // update data user
-        // coba akses model UserModel
-        $user =UserModel:: all(); // ambil semua data dari tabel m_user
+        UserModel::create($data); 
+        
+        $user = UserModel::all(); // ambil semua data dari tabel m_user
         return view('user', ['data' => $user]);
     }   
 }
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -13,4 +13,5 @@ class UserModel extends Model
     protected $primaryKey = 'user_id'; // Mendefinisikan primary key dari tabel yang digunakan
 
     
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
 }
